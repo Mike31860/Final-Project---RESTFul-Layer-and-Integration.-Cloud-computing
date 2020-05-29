59db2257eab754eac5060d0379f7d783947df9f2
Ana Banana <miguel980429@gmail.com>
2020-05-29 15:23:44 -0500

    Time Control Test

--- src/main/java/com/example/demo/Service/TimeControlServiceImpt.java
+++ src/main/java/com/example/demo/Service/TimeControlServiceImpt.java
@@ -6,56 +6,60 @@ import java.time.LocalTime;
 import javax.persistence.Column;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.DAOS.TimeControllerDao;
 import com.example.demo.DAOS.TopicDao;
 import com.example.demo.Model.TsscTimecontrol;
 import com.example.demo.Model.TsscTopic;
 
+@Service
 public class TimeControlServiceImpt implements TimeControlService {
 
 	@Autowired
 	private TimeControllerDao dao;
 	
+	@Autowired
 	public TimeControlServiceImpt(TimeControllerDao dao) {
 		super();
 		this.dao = dao;
 	}
 
 	@Override
+	@Transactional(readOnly=false, propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
 	public TsscTimecontrol AnadirTimeControl(TsscTimecontrol time) {
-		
-		TsscTimecontrol as = null;
-
-		if (time!=null&&!time.getName().equals("") && time.getTsscGame()!=null) {
-			dao.save(as);
-			as = time;
-		}
-
-		return as;
+	
+	
+		return dao.save(time);
 		
 
 	}
 
 	@Override
+	@Transactional(readOnly=false, propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
 	public TsscTimecontrol ActualizarTimeControl(TsscTimecontrol time) {
 		// TODO Auto-generated method stub
 		return dao.merge(time);
 	}
 
 	@Override
+	@Transactional(readOnly=false, propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
 	public TsscTimecontrol findTimeById(Long id) {
 		// TODO Auto-generated method stub
 		return dao.findById(id);
 	}
 
 	@Override
+	@Transactional(readOnly=false, propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
 	public boolean existeById(Long id) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
+	@Transactional(readOnly=false, propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
 	public Iterable<TsscTimecontrol> findAlll() {
 		// TODO Auto-generated method stub
 		return dao.findAll();
@@ -64,6 +68,7 @@ public class TimeControlServiceImpt implements TimeControlService {
 
 
 	@Override
+	@Transactional(readOnly=false, propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
 	public void eliminarTime(TsscTimecontrol time) {
 		dao.delete(time);
 		
--- src/test/java/com/example/demo/Delegate/timeControllDelegateTest.java
+++ src/test/java/com/example/demo/Delegate/timeControllDelegateTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.Delegate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.time.LocalTime;
@@ -11,18 +12,21 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.DAOS.TimeControllerDaoImpt;
 import com.example.demo.Delagate.TimeControllerDelegate;
 import com.example.demo.Model.TsscTimecontrol;
+import com.example.demo.Service.TimeControlServiceImpt;
 
+@SpringBootTest
 class timeControllDelegateTest {
 	
 	@Autowired
 	@InjectMocks
-	private TimeControllerDaoImpt TimeServiceImp;
+	private TimeControlServiceImpt TimeServiceImp;
 	
 	@Mock
 	private TimeControllerDelegate delegate;
@@ -47,10 +51,8 @@ class timeControllDelegateTest {
 		nuevo.setIntervalRunning(BigDecimal.TEN);
 		nuevo.setType("ME");
 		nuevo.setTimeInterval(BigDecimal.valueOf(23));
-		
-
-		TimeServiceImp.save(nuevo);
-		assertNotNull(TimeServiceImp.findById(nuevo.getId()));
+		when(delegate.guardar(nuevo)).thenReturn(nuevo);
+		assertNotNull(TimeServiceImp.AnadirTimeControl(nuevo));
 		
 	}
 	
@@ -67,10 +69,11 @@ class timeControllDelegateTest {
 		nuevo.setIntervalRunning(BigDecimal.TEN);
 		nuevo.setType("ME");
 		nuevo.setTimeInterval(BigDecimal.valueOf(23));
-		TimeServiceImp.save(nuevo);
-		assertNotNull(TimeServiceImp.findById(nuevo.getId()));
-		TimeServiceImp.delete(nuevo);
-		assertNull(TimeServiceImp.findById(nuevo.getId()));
+		when(delegate.guardar(nuevo)).thenReturn(nuevo);
+		assertNotNull(TimeServiceImp.AnadirTimeControl(nuevo));
+		delegate.eliminar(nuevo);
+		TimeServiceImp.eliminarTime(nuevo);
+		assertNull(TimeServiceImp.findTimeById(nuevo.getId()));
 		
 		
 	}
@@ -88,7 +91,9 @@ class timeControllDelegateTest {
 		nuevo.setIntervalRunning(BigDecimal.TEN);
 		nuevo.setType("ME");
 		nuevo.setTimeInterval(BigDecimal.valueOf(23));
-		TimeServiceImp.save(nuevo);
+		when(delegate.guardar(nuevo)).thenReturn(nuevo);
+		assertNotNull(TimeServiceImp.AnadirTimeControl(nuevo));
+		assertNotNull(TimeServiceImp.findTimeById(nuevo.getId()));
 		nuevo.setAutostart("Nelson");
 		nuevo.setOrder(BigDecimal.valueOf(85));
 		nuevo.setLastPlayTime(LocalTime.now());
@@ -97,17 +102,15 @@ class timeControllDelegateTest {
 		nuevo.setIntervalRunning(BigDecimal.valueOf(75));
 		nuevo.setType("MEyu");
 		nuevo.setTimeInterval(BigDecimal.valueOf(29));
-		
-		TimeServiceImp.merge(nuevo);
-		
-		assertEquals(nuevo.getAutostart(),TimeServiceImp.findById(nuevo.getId()).getAutostart());
-		assertEquals(nuevo.getOrder(),TimeServiceImp.findById(nuevo.getId()).getOrder());
-		assertEquals(nuevo.getLastPlayTime(),TimeServiceImp.findById(nuevo.getId()).getLastPlayTime());
-		assertEquals(nuevo.getName(),TimeServiceImp.findById(nuevo.getId()).getName());
-		assertEquals(nuevo.getState(),TimeServiceImp.findById(nuevo.getId()).getState());
-		assertEquals(nuevo.getIntervalRunning(),TimeServiceImp.findById(nuevo.getId()).getIntervalRunning());
-		assertEquals(nuevo.getType(),TimeServiceImp.findById(nuevo.getId()).getType());
-		assertEquals(nuevo.getTimeInterval(),TimeServiceImp.findById(nuevo.getId()).getTimeInterval());
+		when(delegate.actualizar(nuevo)).thenReturn(nuevo);
+		assertEquals(nuevo.getAutostart(),TimeServiceImp.findTimeById(nuevo.getId()).getAutostart());
+		assertEquals(nuevo.getOrder(),TimeServiceImp.findTimeById(nuevo.getId()).getOrder());
+		assertEquals(nuevo.getLastPlayTime(),TimeServiceImp.findTimeById(nuevo.getId()).getLastPlayTime());
+		assertEquals(nuevo.getName(),TimeServiceImp.findTimeById(nuevo.getId()).getName());
+		assertEquals(nuevo.getState(),TimeServiceImp.findTimeById(nuevo.getId()).getState());
+		assertEquals(nuevo.getIntervalRunning(),TimeServiceImp.findTimeById(nuevo.getId()).getIntervalRunning());
+		assertEquals(nuevo.getType(),TimeServiceImp.findTimeById(nuevo.getId()).getType());
+		assertEquals(nuevo.getTimeInterval(),TimeServiceImp.findTimeById(nuevo.getId()).getTimeInterval());
 		
 		
 	}
